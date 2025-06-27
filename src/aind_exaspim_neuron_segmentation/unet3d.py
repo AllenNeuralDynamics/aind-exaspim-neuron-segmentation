be13260423a65e7ec9c6de5c0e815866e52fc4ac
Anna Grim <108307071+anna-grim@users.noreply.github.com>
2025-06-27 15:40:33 -0700

    add: machine learning routines

---
+++ src/aind_exaspim_neuron_segmentation/unet3d.py
@@ -0,0 +1,141 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class UNet(nn.Module):
+    def __init__(self, width_multiplier=1, trilinear=True):
+        # Call parent class
+        super(UNet, self).__init__()
+
+        # Initializations
+        _channels = (16, 32, 64, 128, 256)
+        factor = 2 if trilinear else 1
+
+        # Instance attributes
+        self.channels = [int(c * width_multiplier) for c in _channels]
+        self.trilinear = trilinear
+
+        # Contracting layers
+        self.inc = DoubleConv(1, self.channels[0])
+        self.down1 = Down(self.channels[0], self.channels[1])
+        self.down2 = Down(self.channels[1], self.channels[2])
+        self.down3 = Down(self.channels[2], self.channels[3])
+        self.down4 = Down(self.channels[3], self.channels[4] // factor)
+
+        # Expanding layers
+        self.up1 = Up(self.channels[4], self.channels[3] // factor, trilinear)
+        self.up2 = Up(self.channels[3], self.channels[2] // factor, trilinear)
+        self.up3 = Up(self.channels[2], self.channels[1] // factor, trilinear)
+        self.up4 = Up(self.channels[1], self.channels[0], trilinear)
+        self.outc = OutConv(self.channels[0], 1)
+
+    def forward(self, x):
+        # Contracting layers
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+
+        # Expanding layers
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.outc(x)
+        return logits
+
+
+class DoubleConv(nn.Module):
+    """(convolution => [BN] => ReLU) * 2"""
+
+    def __init__(self, in_channels, out_channels, mid_channels=None):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+        self.double_conv = nn.Sequential(
+            nn.Conv3d(in_channels, mid_channels, kernel_size=3, padding=1),
+            nn.BatchNorm3d(mid_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv3d(mid_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm3d(out_channels),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+
+class Down(nn.Module):
+    """Downscaling with maxpool then double conv"""
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool3d(2), DoubleConv(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.maxpool_conv(x)
+
+
+class Up(nn.Module):
+    """Upscaling then double conv"""
+
+    def __init__(self, in_channels, out_channels, trilinear=True):
+        super().__init__()
+        if trilinear:
+            self.up = nn.Upsample(
+                scale_factor=2, mode="trilinear", align_corners=True
+            )
+            self.conv = DoubleConv(
+                in_channels, out_channels, mid_channels=in_channels // 2
+            )
+        else:
+            self.up = nn.ConvTranspose3d(
+                in_channels, in_channels // 2, kernel_size=2, stride=2
+            )
+            self.conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        # input is CHW
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        x1 = F.pad(
+            x1,
+            [diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2],
+        )
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+
+
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv3d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class DepthwiseSeparableConv3d(nn.Module):
+    def __init__(self, nin, nout, kernel_size, padding, kernels_per_layer=1):
+        super(DepthwiseSeparableConv3d, self).__init__()
+        self.depthwise = nn.Conv3d(
+            nin,
+            nin * kernels_per_layer,
+            kernel_size=kernel_size,
+            padding=padding,
+            groups=nin,
+        )
+        self.pointwise = nn.Conv3d(
+            nin * kernels_per_layer, nout, kernel_size=1
+        )
+
+    def forward(self, x):
+        out = self.depthwise(x)
+        out = self.pointwise(out)
+        return out
